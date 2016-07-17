5babfbd45a3ebe1fc2e115ed018db27b600e2dd5
myfreeer <myfreeer@gmail.com>
2016-07-17 17:13:07 +0800

    Update text2png.cs

--- text2png.cs
+++ text2png.cs
@@ -12,17 +12,19 @@ namespace TextToImageVector
     {
         static void Main(string[] args)
         {
-        	System.Console.WriteLine("parameter count = {0}", args.Length);
+            //usage:
+            //text2png string
+            //text2png string width height
+            System.Console.WriteLine("parameter count = {0}", args.Length);
             string text="";
             int w=950;
             int h=500;
             for (int i = 0; i < args.Length; i++) {System.Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);}
-try{text=args[0];} catch{text="error, no arguments.";}
-try{w=Convert.ToInt32(args[1]);h=Convert.ToInt32(args[2]);}catch{}
+            try{text=args[0];} catch{text="error, no arguments.";}
+            try{w=Convert.ToInt32(args[1]);h=Convert.ToInt32(args[2]);}catch{}
             var bitmapimage = new Bitmap(w, h);
             Graphics bitmapGraphics = Graphics.FromImage(bitmapimage);
             bitmapGraphics.Clear(Color.Black);
-            
             bitmapGraphics.DrawString(text, new Font("Arial", 50, GraphicsUnit.Pixel), Brushes.White, new RectangleF(16, 16,w,h));
             bitmapimage.Save("Image.png", System.Drawing.Imaging.ImageFormat.Png);
         }
